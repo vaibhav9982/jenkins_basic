7f875e7b45bb1a3f17a9c2cb84c73ab8de285a1c
Vaibhav Sharma <sharmavaibhav9982@gmail.com>
2026-08-07 11:13:39 +0530

    added test case

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const data = require("./data.json")
 const fs = require("fs");
 
 let passed = true;
@@ -14,24 +15,87 @@ else{
     console.log("TC 01 : index.html exist :fail");
     passed = false;
 }
-// TC 02
-if(fs.existsSync("style.css"))
-{
-    console.log("TC 02 : style.css exist : Pass");
-}
-else{
-      console.log("TC 01 : index.html exist :fail");
-    passed = false;
+    // TC 02
+    if(fs.existsSync("style.css"))
+    {
+        console.log("TC 02 : style.css exist : Pass");
+    }
+    else{
+        console.log("TC 01 : index.html exist :fail");
+        passed = false;
 
-}
-// TC 03
-if(fs.existsSync("data.json"))
+    }
+    // TC 03
+    if(fs.existsSync("data.json"))
+    {
+        console.log("TC 03 : data.json file exist : Pass");
+    }
+    else
+    {
+        console.log("TC 03 : data.json file exist : Fail");
+        passed = false;
+    }
+
+
+// TC 06 Name validation
+if(data.name.trim()!=="")
 {
-    console.log("TC 03 : data.json file exit : Pass");
+        console.log("TC 05 Name validation : Pass");
 }
 else
 {
-    console.log("TC 03 : data.json file exit : Fail");
-    passed = false;
+console.log("TC 05 Name validation : Fail");
+}
+// TC 07 Emal validation
+
+if(data.email.includes("@"))
+{
+    console.log("TC 08 email validation : Pass");
+}
+else{
+console.log("TC 08 email validation : Fail");
 }
 
+
+// TC 08 mobile validaition..
+if(data.phone.length == 10)
+{
+     console.log("TC 08 mobile no validation : Pass");
+}
+else
+{
+     console.log("TC 08 mobile no validation : Fali");
+}
+
+
+//TC 09 branch validation
+if(data.branch!=="")
+{
+        console.log("TC 09 branch validation :Pass ");
+}
+else
+{
+console.log("TC 09 branch validation :Fail ");
+}
+
+// TC 10 password validation
+if(data.password.length >=6)
+{
+
+    console.log("TC 10 password validaion : Pass");
+}
+else{
+    console.log("TC 10 password validaion : Fail");
+}
+
+
+// //TC 05 Name validation
+// const name = "vaibhav sharma";
+// if(data.name == name)
+// {
+//     console.log(`${name} record exist`);
+// }
+// else
+// {
+//     console.log("rahul sharma record does not exist");
+// }
