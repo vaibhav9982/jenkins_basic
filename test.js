11988d38312cd3cfbdb3443beffd1701397fdf48
Vaibhav Sharma <sharmavaibhav9982@gmail.com>
2026-08-07 10:43:20 +0530

    added test case

--- test.js
+++ test.js
@@ -14,3 +14,24 @@ else{
     console.log("TC 01 : index.html exist :fail");
     passed = false;
 }
+// TC 02
+if(fs.existsSync("style.css"))
+{
+    console.log("TC 02 : style.css exist : Pass");
+}
+else{
+      console.log("TC 01 : index.html exist :fail");
+    passed = false;
+
+}
+// TC 03
+if(fs.existsSync("data.json"))
+{
+    console.log("TC 03 : data.json file exit : Pass");
+}
+else
+{
+    console.log("TC 03 : data.json file exit : Fail");
+    passed = false;
+}
+
